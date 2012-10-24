96fe2186ade753835ad99330324c565b3a298b3e
Humphrey Hu <humphrey.hu@gmail.com>
2012-10-23 21:57:39 -0400

    Commit of file needed for keyboard wire

---
+++ lib/network_const.py
@@ -0,0 +1,24 @@
+# Network Constants defined here
+
+# Bootloader constants
+BOOTLOADER_SERVER_ADDR = 0x1020
+BOOTLOADER_SERVER_PAN = 0x1000
+BOOTLOADER_CLIENT_ADDR = 0x1101
+BOOTLOADER_CLIENT_PAN = 0x1000
+BOOTLOADER_CHANNEL = 0x15
+
+# Network discovery/assignment constants
+DISCOVERY_SERVER_ADDR = 0x1020
+DISCOVERY_SERVER_PAN = 0x1001
+DISCOVERY_CLIENT_ADDR = 0x1101
+DISCOVERY_CLIENT_PAN = 0x1001
+DISCOVERY_CHANNEL = 0x15 # Not the TV channel...
+
+# Active formation network constants
+# Currently the same as the discovery network due to XBee
+# PAN addressing limitations
+ACTIVE_SERVER_ADDR = 0x1020
+ACTIVE_SERVER_PAN = 0x1001
+ACTIVE_CLIENT_PAN = 0x1001
+ACTIVE_CHANNEL = 0x15
+
